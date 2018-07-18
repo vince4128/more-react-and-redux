2bf86091618c1ba78ec0a832e9931d49df0419a4
vince4128 <vrlt.vincent@gmail.com>
2018-07-18 11:56:07 +0200

    first commit and first test

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+it('shows a comment box', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<App />, div);
+
+    // Looks inside the div
+    // and checks to see if the CommentBox is in there
+    console.log(div.innerHTML);
+
+    // cleanup
+    ReactDOM.unmountComponentAtNode(div);
+});

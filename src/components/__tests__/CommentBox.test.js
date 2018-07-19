4e2b87a74afe3a80726c2089792ea454aed3b455
vince4128 <vrlt.vincent@gmail.com>
2018-07-19 15:22:59 +0200

    test grouped in describe

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,25 @@ it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
+
+describe('the text area', () => {
+    
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change',{
+            target: {value: 'new comment'}
+        });
+        wrapped.update();
+    });
+
+    it('has a text area that users can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    });
+
+    it('when form is submitted, text area get emptied', ()=>{
+        wrapped.find('form').simulate('submit');
+        wrapped.update();
+
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+
+});

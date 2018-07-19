9fb4627669bc1a5714a3071b2d3e923a01400469
vince4128 <vrlt.vincent@gmail.com>
2018-07-19 17:46:01 +0200

    initialState as a props added

--- src/Root.js
+++ src/Root.js
@@ -3,10 +3,10 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from 'reducers';
 
-export default props => {
+export default ({ children, initialState = {} }) => {
     return(
-        <Provider store={createStore(reducers, {})}>
-            {props.children}
+        <Provider store={createStore(reducers, initialState)}>
+            {children}
             {/* props.children allow to take a component and use it to wrap other components */}
             {/* so by using it into index.js it's like <App/> was wrapped here */}
         </Provider>

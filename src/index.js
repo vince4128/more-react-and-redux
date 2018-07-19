32ca3314a8f2d43529f6547c057fa87c48996b03
vince4128 <vrlt.vincent@gmail.com>
2018-07-19 15:59:20 +0200

    redux added (test broken)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from 'reducers';
 
 import App from 'components/App';
 
 ReactDOM.render(
-    <App />, document.querySelector('#root')
+    <Provider store={createStore(reducers, {})}>
+        <App />
+    </Provider>
+    , document.querySelector('#root')
 );

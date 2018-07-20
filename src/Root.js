45d1201fb6d6c322658931aeccae842d7093c92f
vince4128 <vrlt.vincent@gmail.com>
2018-07-20 14:39:10 +0200

    adding redux-promise middleware

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
 export default ({ children, initialState = {} }) => {
+    const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
     return(
-        <Provider store={createStore(reducers, initialState)}>
+        <Provider store={store}>
             {children}
             {/* props.children allow to take a component and use it to wrap other components */}
             {/* so by using it into index.js it's like <App/> was wrapped here */}

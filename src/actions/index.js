1effaf43f49fa07d8d9ace84c94ddc5ab583d122
vince4128 <vrlt.vincent@gmail.com>
2018-07-20 16:47:24 +0200

    fake Auth added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { SAVE_COMMENT } from 'actions/types';
-import { FETCH_COMMENTS } from './types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 
 export function saveComment(comment){
     return {
@@ -15,5 +14,12 @@ export function fetchComments(){
     return {
         type: FETCH_COMMENTS,
         payload: response
+    }
+}
+
+export function changeAuth(isLoggedIn){
+    return {
+        type:CHANGE_AUTH,
+        payload:isLoggedIn
     }
 }

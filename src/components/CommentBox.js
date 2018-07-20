12fc34b060b1635eac54f6a3559be755207dec61
vince4128 <vrlt.vincent@gmail.com>
2018-07-20 17:18:40 +0200

    reusable logic moved inside requireAuth component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -6,22 +6,6 @@ class CommentBox extends Component {
 
     state = { comment:  ''}
 
-    // Our component just got rendered
-    componentDidMount(){
-        this.shouldNavigateAway();
-    }
-
-    // Component receive a new set of props
-    componentDidUpdate(){
-        this.shouldNavigateAway();
-    }
-
-    shouldNavigateAway(){
-        if(!this.props.auth){
-            this.props.history.push('/');
-        }
-    }
-
     handleChange = event => {
         this.setState({comment: event.target.value});
     };
@@ -49,8 +33,4 @@ class CommentBox extends Component {
     }
 }
 
-function mapStateToProps(state) {
-    return { auth: state.auth };
-}
-
-export default connect(mapStateToProps, actions)(CommentBox);
+export default connect(null, actions)(CommentBox);
--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import connect from 'react-redux';
 
 export default (ChildComponent) => {
     class ComposedComponent extends Component {
+
+        // Our component just got rendered
+        componentDidMount(){
+            this.shouldNavigateAway();
+        }
+
+        // Component receive a new set of props
+        componentDidUpdate(){
+            this.shouldNavigateAway();
+        }
+
+        shouldNavigateAway(){
+            if(!this.props.auth){
+                this.props.history.push('/');
+            }
+        }
+
         render(){
             return <ChildComponent />
         }
     }
 
-    return ComposedComponent;
+    function mapStateToProps(state) {
+        return { auth: state.auth };
+    }
+
+    return connect(mapStateToProps)(ComposedComponent);
 };

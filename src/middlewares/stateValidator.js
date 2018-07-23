bca3d04a8a9e899e253a3ac517082102f92617cc
vince4128 <vrlt.vincent@gmail.com>
2018-07-23 12:01:28 +0200

    emitting warnings

--- src/middlewares/stateValidator.js
+++ src/middlewares/stateValidator.js
@@ -1,9 +1,11 @@
 import tv4 from 'tv4';
 import stateSchema from './stateSchema';
 
-export default ({ dispatch }) => (next) => (action) => {
+export default ({ dispatch, getState }) => (next) => (action) => {
     next(action);
 
-    console.log(tv4.validate(getState(), stateSchema));
+    if(!tv4.validate(getState(), stateSchema)){
+        console.warn('Invalid state schema detected !');
+    };
 };
 
